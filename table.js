50051574d703a469ad032bdb417d032f2cd690ec
file-gugugu <15601593268@163.com>
2024-07-14 00:22:09 +0800

    第二次提交

---
+++ table.js
@@ -0,0 +1,9 @@
+function adddata(){
+    var table=document.getElementById("idtable");
+    //console.log("success");
+    var length=table.rows.length;
+    var newrow=table.insertRow(length);
+    var namecol=newrow.insertCell(0);
+    var phonecol=newrow.insertCell(1);
+    var actioncol=newrow.insertCell(2) 
+}
