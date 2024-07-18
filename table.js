afe9b2d48c7ab8514ff19cd820ecef81f23d1284
file-gugugu <15601593268@163.com>
2024-07-18 22:30:28 +0800

    7.18修改

--- table.js
+++ table.js
@@ -5,5 +5,10 @@ function adddata(){
     var newrow=table.insertRow(length);
     var namecol=newrow.insertCell(0);
     var phonecol=newrow.insertCell(1);
-    var actioncol=newrow.insertCell(2) /**/ 
+    var actioncol=newrow.insertCell(2);
+    //console.log(newrow);
+    namecol.innerHTML = "未命名";
+    phonecol.innerHTML = '无联系方式';
+    actioncol.innerHTML = '<button>编辑</button><button>删除</button>';
+  
 }
